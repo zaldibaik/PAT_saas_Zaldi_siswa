2f38be88c7d8cc7727a22727f435d6bd0e2b19cc
zzaldi <zaldizaldizaldi032@gmail.com>
2024-06-07 14:36:05 +0700

    Update

--- src/Component/myheader.js
+++ src/Component/myheader.js
@@ -8,7 +8,7 @@ class Myheader extends Component {
       <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href="#"
             class="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img src={Logo} class="h-8 w-40 h-auto" alt="Flowbite Logo" />
